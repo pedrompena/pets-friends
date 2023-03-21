a32ab26cb9b10abdfee59b2b74620f3784f2d1bc
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-03-21 11:55:29 +0000

    bcknd url

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,8 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
   return {
     store: {
-      BACKEND_URL:
-        "https://3001-s3rtr3s-petsfriends-00lslwimxf5.ws-eu90.gitpod.io/",
+      BACKEND_URL: process.env.BACKEND_URL,
       clientInfo: {},
     },
     actions: {
@@ -15,22 +14,25 @@ const getState = ({ getStore, getActions, setStore }) => {
         setStore({ ...getStore(), clientInfo: {} });
       },
       setLocalStorage: (client) => {
-        localStorage.setItem("clientInfo", JSON.stringify(client))
+        localStorage.setItem("clientInfo", JSON.stringify(client));
       },
       uploadImage: async (file) => {
         const formData = new FormData();
-        formData.append('file', file);
-        formData.append('client_id', getStore().clientInfo.id);
-        
+        formData.append("file", file);
+        formData.append("client_id", getStore().clientInfo.id);
+
         const options = {
-          method: 'POST',
+          method: "POST",
           body: formData,
         };
-        
-        const resp = await fetch(getStore().BACKEND_URL + "api/upload", options)
-        const data = await resp.json()
-        return data
-      }
+
+        const resp = await fetch(
+          getStore().BACKEND_URL + "api/upload",
+          options
+        );
+        const data = await resp.json();
+        return data;
+      },
     },
   };
 };
