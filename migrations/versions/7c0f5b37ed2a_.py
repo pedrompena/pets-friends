5eba3f91a81f21ed4cfdc14f6bfbd4ec0a6b7dd6
pepemiguelx2 <pepemiguelx2@gmail.com>
2023-04-10 09:19:16 +0000

    migrations

--- migrations/versions/7c0f5b37ed2a_.py
+++ migrations/versions/7c0f5b37ed2a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: aaa441346de8
+Revision ID: 7c0f5b37ed2a
 Revises: 
-Create Date: 2023-04-04 15:47:39.793449
+Create Date: 2023-04-10 09:18:54.603122
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'aaa441346de8'
+revision = '7c0f5b37ed2a'
 down_revision = None
 branch_labels = None
 depends_on = None
